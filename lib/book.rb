df778fabf09b4fafb7d882902efe1d6324f26594
David Cantoran <davidcantoran36@gmail.com>
2019-03-26 15:30:17 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,23 @@
 class Book
   
-  def initialize(title)
+  def initialize(title, author, page, genre )
+    
     @title = title
+    @author = author 
+    @page = page 
+    @genre = genre 
   end
   def title
     @title
+  end
+  def author 
+    @author 
+  end 
+  def page 
+    @page 
+  end 
+  def genre 
+    @genre 
   end
   
 end 
